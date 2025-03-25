80c650c1cd9afa90f51a5d74363eda197bb06a1b
adam5096 <adamchou5096@gmail.com>
2025-03-25 19:26:30 +0800

    update: adjust postgreSQL config

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ const swaggerFile = require("./swagger_output.json"); // 剛剛輸出的 JSON
 
 // 設置 PostgreSQL 連接
 const pool = new Pool({
-  user: process.env.PGUSER || "postgres",
-  host: process.env.PGHOST || "localhost",
-  database: process.env.PGDATABASE || "auth_api",
-  password: process.env.PGPASSWORD || "postgres",
-  port: process.env.PGPORT || 5432,
+  user: process.env.PGUSER ,
+  host: process.env.PGHOST ,
+  database: process.env.PGDATABASE ,
+  password: process.env.PGPASSWORD ,
+  port: process.env.PGPORT,
 });
 
 // 測試數據庫連接
